4a14e14031bf2853f833fea99182a597aae32443
AlisonClark <alison.marie.clark@gmail.com>
2014-05-20 22:11:37 -0700

    added fields to StockWatcher.java

--- src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
+++ src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -1,5 +1,7 @@
 package com.google.gwt.sample.stockwatcher.client;
 
+import java.util.ArrayList;
+
 import com.google.gwt.sample.stockwatcher.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
@@ -11,7 +13,9 @@ import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
@@ -29,6 +33,14 @@ public class StockWatcher implements EntryPoint {
 			+ "attempting to contact the server. Please check your network "
 			+ "connection and try again.";
 
+	private static final int REFRESH_INTERVAL = 5000; // ms
+	private VerticalPanel mainPanel = new VerticalPanel(); 
+	private FlexTable stocksFlexTable = new FlexTable(); 
+	private HorizontalPanel addPanel = new HorizontalPanel(); 
+	private TextBox newSymbolTextBox = new TextBox(); 
+	private Button addStockButton = new Button("Add"); 
+	private Label lastUpdatedLabel = new Label();
+	private ArrayList<String> stocks = new ArrayList<String>();
 	/**
 	 * Create a remote service proxy to talk to the server-side Greeting service.
 	 */
